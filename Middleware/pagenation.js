952c26969b0967dabb101c348625308430aac43b
rahul <rahulbhargavrgk@gmail.com>
2024-12-07 18:02:07 +0530

    added search by topics method

--- Middleware/pagenation.js
+++ Middleware/pagenation.js
@@ -1,8 +1,14 @@
+
+
 export function pagenate(model){
     return async (req,res,next)=>{
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 10;
-        const order = parseInt(req.query.order) || 1
+        const order = parseInt(req.query.order) || 1;
+        const sortBy=[]
+        if(req.query.sortBy){
+            sortBy.push(req.query.sortBy)
+        }
         const count = await model.countDocuments();
 
         const startIndex = (page-1)*limit;
@@ -21,7 +27,35 @@ export function pagenate(model){
                 limit:limit
             }
         }
+      
 
+        if(sortBy.length !=0){
+            try{
+                results.results = await model.aggregate([
+                    {
+                        $match:{ tags:{$in:sortBy}}
+                    },
+                    {
+                        $sort:{difficulty:order}
+                    },
+                    {
+                        $project:{_id:0}
+                    },
+                    {
+                        $skip:startIndex
+                    },
+                    {
+                        $limit:limit
+                    }
+                ])
+                res.pagenatedResults = results;
+                return next();
+            }
+            catch(error){
+                return res.status(500).end(`error:${error.message}`)
+            }
+        }
+        
         try{
             results.results = await model.aggregate([
                 {
